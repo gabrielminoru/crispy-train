849d4f184817c83b52b9041d49d5b82eaa89e3b0
minoru <gabrielikenaga@gmail.com>
2023-01-24 22:41:04 -0300

    mais ocisa

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from ct import data, gpt3, prompt, searcher
 from ct.data import StopWords, Data
 from ct.gpt3 import Chat
 from ct.searcher import DumbSearcher
@@ -8,14 +7,14 @@ from config import settings
 
 
 # getting data
-stopwords = data.StopWords(Path('stopwords.txt'))
-data = data.Data(Path('data/data.txt'), stopwords)
+stopwords = StopWords(Path('stopwords.txt'))
+data = Data(Path('data/data.txt'), stopwords)
 
 # setting up searcher, will use only 15 paragraphs
-searcher = searcher.DumbSearcher(data, limiter = 15)
+searcher = DumbSearcher(data, limiter = 15)
 
 # initializing chat
-chat = gpt3.Chat(settings, botname = "Ishido")
+chat = Chat(settings, botname = "Ishido")
 chat.set_searcher(searcher = searcher)
 
 if __name__ == "__main__":
